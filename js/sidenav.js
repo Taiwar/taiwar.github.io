82eae2d4d696a14fc4f084397c20e644965af34b
Jonas <jonas.mueller.contact@gmail.com>
2018-09-08 01:29:39 +0200

    Redesign project cards to have small explanation collapsible.

--- js/sidenav.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let elems = document.querySelectorAll('.sidenav');
-    let instances = M.Sidenav.init(elems, {
-        menuWidth: 250,
-        draggable: true,
-        closeOnClick: true,
-    });
-});
---
+++ js/style_inits.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let sidenav_elems = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav_elems, {
+        menuWidth: 250,
+        draggable: true,
+        closeOnClick: true,
+    });
+    let collapsible_elems = document.getElementsByClassName("collapsed");
+    console.log(collapsible_elems);
+    Array.from(collapsible_elems).forEach((elem) => {
+        elem.addEventListener("click", function() {
+            this.classList.toggle("active");
+            let content = this.nextElementSibling;
+            if (content.style.maxHeight) {
+                content.style.maxHeight = null;
+                elem.firstChild.innerHTML = "expand_more"
+            } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+                elem.firstChild.innerHTML = "expand_less"
+            }
+        });
+    });
+});
