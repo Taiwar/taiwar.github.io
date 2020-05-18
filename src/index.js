5e3b1eb065ae78c60056ab425473f5d65991b16b
Jonas Müller <jonas.mueller.contact@gmail.com>
2020-05-18 23:55:28 +0200

    Basic styling with semantic.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import 'preact/debug';
+import 'semantic-ui-css/semantic.min.css'
 import './style';
 import App from './components/app';
 
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,11 +1,13 @@
 import style from './style';
 import logo from '../../assets/icons/icon-512.png';
+import { Container, Header } from 'semantic-ui-react'
 
 const Home = () => (
 	<div class={style.home}>
-		<h1>Home</h1>
-		<p>Under construction</p>
-		<img src={logo} alt="Taiwar logo" />
+		<Container class={style.highlight}>
+			<img class={style.logo} src={logo} alt="Taiwar logo" />
+			<Header class="center aligned" as='h1'>Taiwar</Header>
+		</Container>
 	</div>
 );
 
